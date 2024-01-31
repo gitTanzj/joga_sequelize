e468f90a5c156fd2b62404dbb2ddcfa40bab8843
gitTanzj <kalle.riit@gmail.com>
2024-01-31 08:54:27 +0200

    Installed basic packages

---
+++ indes.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const PORT = 3000
+
+app.use(express.json())
+
+app.use(express.urlencoded({ extended:true }))
+
+app.get('/', (req, res) => {
+    res.json({ message: "Welcome to sequelize application." })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
