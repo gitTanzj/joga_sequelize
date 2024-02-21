d44a7b05e3afcca16d8c7cca80ca9a5dcd18d94a
gitTanzj <kalle.riit@gmail.com>
2024-02-21 11:27:43 +0200

    Created new article

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ const authorRouter = require('./routes/author')
 app.use('/', articleRouter)
 app.use('/article', articleRouter)
 app.use('/author', authorRouter)
+app.use('/admin/article', articleRouter)
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
